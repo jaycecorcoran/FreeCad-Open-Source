237098abbe0f318e9c85921da384380158f149ca
Jayce Corcoran <47369842+jaycecorcoran@users.noreply.github.com>
2025-02-17 22:01:59 -0500

    Add files via upload

--- TaskMeasure.cpp
+++ TaskMeasure.cpp
@@ -72,7 +72,7 @@ TaskMeasure::TaskMeasure()
 
     QSettings settings;
     settings.beginGroup(QLatin1String(taskMeasureSettingsGroup));
-    delta = settings.value(QLatin1String(taskMeasureShowDeltaSettingsName), true).toBool(); //changed delta parameter to true
+    delta = settings.value(QLatin1String(taskMeasureShowDeltaSettingsName), delta).toBool();
     mAutoSave = settings.value(QLatin1String(taskMeasureAutoSaveSettingsName), mAutoSave).toBool();
     if (settings.value(QLatin1String(taskMeasureGreedySelection), false).toBool()) {
         Gui::Selection().setSelectionStyle(SelectionStyle::GreedySelection);
@@ -80,7 +80,6 @@ TaskMeasure::TaskMeasure()
     else {
         Gui::Selection().setSelectionStyle(SelectionStyle::NormalSelection);
     }
-    settings.endGroup();
 
     showDelta = new QCheckBox();
     showDelta->setChecked(delta);
@@ -531,8 +530,7 @@ void TaskMeasure::showDeltaChanged(int checkState)
     QSettings settings;
     settings.beginGroup(QLatin1String(taskMeasureSettingsGroup));
     settings.setValue(QLatin1String(taskMeasureShowDeltaSettingsName), delta);
-    settings.endGroup();
-    settings.sync();
+
     this->update();
 }
 
@@ -557,7 +555,6 @@ void TaskMeasure::newMeasurementBehaviourChanged(bool checked)
         Gui::Selection().setSelectionStyle(SelectionStyle::GreedySelection);
         settings.setValue(QLatin1String(taskMeasureGreedySelection), true);
     }
-    settings.endGroup();
 }
 
 void TaskMeasure::setModeSilent(App::MeasureType* mode)
